40fcef19b263d4ba27294afbab73782494038ba0
Noura Ismail <12030827@students.liu.edu.lb>
2024-12-05 23:01:57 +0200

    add employee component

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,10 +1,16 @@
+import { DialogRef } from '@angular/cdk/dialog';
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { EmployeeService } from '../services/employee.service';
 @Component({
   selector: 'app-add-employee',
   imports: [
@@ -14,8 +20,53 @@ import { MatInputModule } from '@angular/material/input';
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    NgFor,
+    ReactiveFormsModule,
   ],
   templateUrl: './add-employee.component.html',
   styleUrl: './add-employee.component.scss',
 })
-export class AddEmployeeComponent {}
+export class AddEmployeeComponent {
+  empForm: FormGroup;
+  education: string[] = [
+    'Matric',
+    'Intermediate',
+    'Bachelor',
+    'Master',
+    'Post Graduate',
+  ];
+  constructor(
+    private _fb: FormBuilder,
+    private _empService: EmployeeService,
+    private _dialogRef: DialogRef<AddEmployeeComponent>
+  ) {
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      gender: '',
+      dob: '',
+      education: '',
+      company: '',
+      experience: '',
+      package: '',
+    });
+  }
+  onSubmit() {
+    if (this.empForm.valid) {
+      this._empService.addEmployee(this.empForm.value).subscribe({
+        next: (val: any) => {
+          alert('Employee added successfully');
+          this._dialogRef.close();
+        },
+        error: () => {
+          console.log('error');
+        },
+      });
+    } else {
+      console.log('Invalid form');
+    }
+  }
+}

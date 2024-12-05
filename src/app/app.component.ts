ea7a492b461e18facfd49718e787f6d0c2674ac3
Noura Ismail <12030827@students.liu.edu.lb>
2024-12-05 23:03:06 +0200

    add employee table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,59 @@
-import { Component, inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EmployeeService } from './services/employee.service';
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, MatIconModule, MatToolbarModule, MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  standalone: false,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+    'firstName',
+    'lastName',
+    'email',
+    'dob',
+    'gender',
+    'education',
+    'company',
+    'experience',
+    'package',
+    'action',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   readonly dialog = inject(MatDialog);
-  title = 'crudProject';
+  private employeeService = inject(EmployeeService);
+
+  ngOnInit(): void {
+    this.getEmployeList();
+  }
   openDialog() {
     this.dialog.open(AddEmployeeComponent);
+  }
+  getEmployeList() {
+    this.employeeService.getEmployeesList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: console.log,
+    });
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
